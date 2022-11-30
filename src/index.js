d3e4028d5fe55dd3c96b16a201d6221ae4330eb5
perym <pery.mimon@gmail.com>
2022-11-30 22:57:08 +0200

    read urls from inline style elements

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Configstore from 'configstore';
 import {join} from 'path';
 import ora from 'ora';
 
-const browser = await puppeteer.launch({headless: true});
+const browser = await puppeteer.launch({headless: false});
 const page = await browser.newPage();
 
 const store = new Configstore('result', {}, {configPath: join(process.cwd(), 'result.json')});
@@ -15,7 +15,7 @@ const [, , argUrl, maxDeep] = process.argv;
 spinner.start('Crawling into the web ...');
 const result = await crawlDeep(argUrl, Number(maxDeep))
 store.set('result', result)
-spinner.succeed('Crawl complate. Check result.json')
+spinner.succeed('Crawl complete. Check result.json')
 await browser.close();
 /* end MAIN CODE */
 
@@ -28,15 +28,21 @@ async function crawlDeep(sourceUrl, maxDeep = 0) {
 
     while (stack.length) {
         const {sourceUrl, deep} = stack.shift();
-        await page.goto(sourceUrl);
+        await page.goto(sourceUrl,{
+            waitUntil:'networkidle2'
+        });
 
         // look for all <img> in the page 
         let imagesURL = await page.$$eval('img', function (imgs) {
             return imgs.map(img => img.src)
         })
 
+        let styleURL = await page.$$eval('[style]', function (elms){
+            return elms.map( e=> e.getAttribute('style')).map(s => s.match(/background.*?url\((.+?)\)/)).filter(Boolean).map( a=> a[1])
+        })
+
         // save all images url in the collection
-        for (let url of imagesURL) {
+        for (let url of [imagesURL, styleURL].flat()) {
             imageDetails.push({
                 imageUrl: url,
                 sourceUrl: sourceUrl,
@@ -50,7 +56,7 @@ async function crawlDeep(sourceUrl, maxDeep = 0) {
                 return a.map(a => a.href)
             })
 
-            // If there are new save them for next check
+            // If there is new links save them for next check
             for (let link of links) {
                 if (link == '') continue;
                 if (urlTagged.has(sigUrl(link))) continue;
@@ -72,7 +78,7 @@ function sigUrl(url) {
         const $url = new URL(url);
         return url.replace($url.hash, '').replace($url.protocol, '');
     } catch (e) {
-        console.error(`url ${url} are not ligal`)
+        console.error(`url ${url} are not legal`)
         debugger
     }
 
